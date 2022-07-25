fd05fa6a5ff42fe05d5a72691dcdaaf47c2d5524
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2022-07-25 19:04:55 -0300

    desafio02

--- desafios/desafio01/funçao.js
+++ desafios/desafio01/funçao.js
@@ -1,18 +1,20 @@
 function carregar(){
     var data = new Date()
     var hora = data.getHours()
-
     var aviso = document.querySelector('div#hr')
     aviso.innerText = (`Agora são ${hora} horas.`)
 
     var foto = document.getElementById('img')
     if(hora >= 0 && hora < 12) {
-        foto.src = 'manha.png'
+        foto.src = 'imagens/manha.png'
+        document.body.style.background = '#d5d32c'
     }
     else if(hora >= 12 && hora < 18) {
-        foto.src = 'tarde.png'
+        foto.src = 'imagens/tarde.png'
+        document.body.style.background = '#f7a489'
     }
     else {
-        foto.src = 'noite.png'
+        foto.src = 'imagens/noite.png'
+        document.body.style.background = '#152e46'
     }
 }
