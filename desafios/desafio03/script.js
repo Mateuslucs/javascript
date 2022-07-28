9dcfd59a68ea6953f65f8c4c7518582104c53b8c
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2022-07-28 17:13:50 -0300

    desafio03

--- desafios/desafio03/script.js
+++ desafios/desafio03/script.js
@@ -4,8 +4,29 @@ function contar(){
     var passo = document.getElementById('pas').value
     var res = document.getElementById('res')
     
-    res.innerText = 'Contando'
-    for(var c = inicio; c <= fim; c += passo){
-        res.innerText = c
+    if(inicio.length == 0 || fim.length == 0 || passo.length == 0){
+        alert('Ta faltando dados')
     }
+    else{
+        if(passo == 0){
+            alert('Passo invalido! considerando passo = 1')
+            p = 1
+        }
+        res.innerHTML = 'contando: <br>'
+        let i = Number(inicio)
+        let f = Number(fim)
+        let p = Number(passo)
+
+        if(i < f){
+            for(var c = i ; c <= f; c += p){
+                res.innerHTML += `${c} `
+            }
+        } else{
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} `
+            }
+        }
+        
+    }
+   
 }
